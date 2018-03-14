e69b919922ede7a02e334a1b40f3be7eb1f7d0f2
Abigail Thelin <abbyjthelin@gmail.com>
2018-03-14 15:18:01 -0600

    parent and children elements are rendered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'rizepoint';
+  menu = [
+    {
+    title: 'Define',
+    children: ['blah', 'blah', 'blah']
+    },
+    {
+      title: 'Gather',
+      children: ['blah', 'blah', 'blah']
+    },
+    {
+      title: 'See',
+      children: ['blah', 'blah', 'blah', 'blah']
+    },    
+    {
+      title: 'Act',
+      children: ['blah', 'blah', 'blah']
+    },
+    {
+      title: 'Admin',
+      children: ['blah', 'blah', 'blah']
+    }
+  ];
+  togglers = {};
+  openChildren(i){
+    console.log(i)
+    // this.togglers[i]=!this.togglers
+  }
 }
