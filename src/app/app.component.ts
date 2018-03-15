54fc3399096cb911e62bb3bac915bad03ab8c733
Abigail Thelin <abbyjthelin@gmail.com>
2018-03-15 12:09:29 -0600

    naming conventions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RIZEPOINT';
+  title = 'RizePoint';
   menu = [
     {
     title: 'Define',
